4f55105cddd09135cb8879f6ec96681866451196
Md. Nayeem <renayeem@gmail.com>
2023-08-17 17:59:21 +0600

    working on navbar

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from './components/navbar/navbar';
+
+const page = () => {
+  return (
+    <>
+      <Navbar></Navbar>
+    </>
+  );
+};
+
+export default page;
