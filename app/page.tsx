2f49690bb1f5ea05db89119c42d8467d746d7c62
Md. Nayeem <renayeem@gmail.com>
2023-08-18 15:17:20 +0600

    working on carousel

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from './components/navbar/navbar';
+import Carousel from './components/carousel/carousel';
 
 const page = () => {
   return (
     <>
       <Navbar></Navbar>
+      <Carousel></Carousel>
     </>
   );
 };
