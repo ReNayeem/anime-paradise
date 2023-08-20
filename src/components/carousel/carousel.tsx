0f21d5b746ececfbab3769841c446ebe98f2c18e
im-mahadi <itz.me.mahadi@gmail.com>
2023-08-20 12:20:49 +0600

    update carousel component
    - add key to carousel div

--- src/components/carousel/carousel.tsx
+++ src/components/carousel/carousel.tsx
@@ -3,31 +3,37 @@
 import Carousel from "react-bootstrap/Carousel";
 import Image from "next/image";
 import { banners } from "@/data/banner.data";
-import './carousel.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import "./carousel.css";
 
-
-
-//TODO: Implement the data with carousel
-function UncontrolledExample() {
+function BannerCarousel() {
   return (
     <div className="custom-carousel">
       <Carousel>
         {banners.map((banner, index) => {
-          return <Carousel.Item>
-            <div className="d-flex container custom-carousel-2">
-              <Image src={banner.image} className="banner-img" width={3000} height={951} alt="" />
-              <div className="d-flex flex-column banner-anime-details">
-                <h3 className="banner-anime-name">{banner.name}</h3>
-                <p className="banner-anime-description">{banner.description.slice(0, 700) + "..."}</p>
-                <button className="banner-btn">Watch now</button>
+          return (
+            <Carousel.Item key={index}>
+              <div className="d-flex container custom-carousel-2">
+                <Image
+                  src={banner.image}
+                  className="banner-img"
+                  width={3000}
+                  height={951}
+                  alt=""
+                />
+                <div className="d-flex flex-column banner-anime-details">
+                  <h3 className="banner-anime-name">{banner.name}</h3>
+                  <p className="banner-anime-description">
+                    {banner.description.slice(0, 700) + "..."}
+                  </p>
+                  <button className="banner-btn">Watch now</button>
+                </div>
               </div>
-            </div>
-          </Carousel.Item>
+            </Carousel.Item>
+          );
         })}
       </Carousel>
     </div>
   );
 }
 
-export default UncontrolledExample;
+export default BannerCarousel;
