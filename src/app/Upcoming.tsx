fa5c06429874cf8078f33d83b02fed2dee880ff1
Md. Nayeem <renayeem@gmail.com>
2023-08-21 16:29:08 +0600

    working on upcoming cards

--- src/app/Upcoming.tsx
+++ src/app/Upcoming.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
 import "./../css/upcoming.css";
+import Image from "next/image";
+import { banners } from "@/data/banner.data";
 
 function Upcoming() {
   return (
@@ -10,6 +12,22 @@ function Upcoming() {
           <Link href="/pages/upcoming">
             <button className="upcoming-btn">See all</button>
           </Link>
+        </div>
+
+        <div className="d-flex flex-wrap justify-content-center gap-3">
+          {banners.slice(0, 4).map((banner, index) => {
+            return (
+              <div className="upcoming-anime">
+                <Image
+                  src={banner.image}
+                  className="upcoming-anime-img"
+                  width={3000}
+                  height={951}
+                  alt=""
+                />
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
