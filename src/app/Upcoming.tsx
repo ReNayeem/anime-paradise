66ea17c8c7383deb634d91dc545d89595dcc8293
Md. Nayeem <renayeem@gmail.com>
2023-08-21 18:17:52 +0600

    trending home section done

--- src/app/Upcoming.tsx
+++ src/app/Upcoming.tsx
@@ -14,7 +14,7 @@ function Upcoming() {
           </Link>
         </div>
 
-        <div className="d-flex flex-wrap justify-content-center gap-3 mb-5">
+        <div className="d-flex flex-wrap justify-content-center gap-3">
           {banners.slice(0, 5).map((banner, index) => {
             return (
               <Link href="">
@@ -31,7 +31,7 @@ function Upcoming() {
                       banner.name.length < 21 ? <h5>{banner.name}</h5> : <h5>{banner.name.slice(0, 21) + "..."}</h5>
                     }
                     <h6>{banner.description.slice(0, 89) + "..."}</h6>
-                    <p>TV <span className="dot"></span> 23m</p>
+                    <p>TV <span className="upcoming-anime-dot"></span> 23m</p>
                   </div>
                 </div>
               </Link>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Carousel from "@/components/carousel";
 import Upcoming from "./Upcoming";
+import Trending from "./Trending";
 
 function Home() {
   return (
     <>
       <Carousel />
       <Upcoming />
+      <Trending />
     </>
   );
 }
